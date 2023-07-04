531d72a305e5c781f673af36a3dc93925bc630cd
Mateo Castano <m.castano1@uniandes.edu.co>
2023-07-04 11:45:34 -0500

    :sparkles: Logic in the app is done

--- app-goals/App.js
+++ app-goals/App.js
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoalText]);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Course Goal"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {(courseGoals || []).map((goal, index) => (
+          <Text key={index}>{goal}</Text>
+        ))}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: "grey",
+    borderBottomWidth: 1,
+    paddingBottom: 20,
+    marginBottom: 10,
+    width: "100%",
+  },
+  textInput: {
+    borderColor: "grey",
+    borderRadius: 5,
+    borderWidth: 0.5,
+    padding: 10,
+    width: "80%",
   },
 });
